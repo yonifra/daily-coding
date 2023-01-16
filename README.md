d6d1e17d863cdc27c95ee9b0f991d6484122441b
Jonathan Fraimorice <jonathanfr@wix.com>
2023-01-16 21:09:05 +0200

    Remove one palindrome challenge

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# daily-coding
-Daily coding challenges to keep me sharp
+# Daily Coding Challenges
+
+Daily coding challenges to keep me sharp.
+After completing three Advent of Code challenges, I thought I might wanna keep myself and my coding skills focused and sharp.
+
+That's why I've signed up for the Daily Byte, which sends you one coding challenge daily. These challenges are not as difficult
+as the ones in the AoC, but they are sweet, and give you a sense of accomplishment once you complete them.
+
+I didn't want this repo to be heavy and cumbersome. You can use the Quokka extension for VS Code to run individual files, and I keep the original
+wording of the challenge for clarity.
+
+These challenges were taken in TypeScript, so I can also brush up on these, but they can be solved in any other programming language out there.
+
+Happy coding!
+
+`¯ \ _ (ツ) _ / ¯`
---
+++ src/removeOnePalindrome.ts
@@ -0,0 +1,34 @@
+// Given a string and the ability to delete at most one character, return whether or not it can form a palindrome.
+// Note: a palindrome is a sequence of characters that reads the same forwards and backwards.
+
+const removeOnePalindrome = (str: String): boolean => {
+  let left: number = 0
+  let right: number = str.length - 1
+  let firstMismatch: boolean = false
+
+  while (left < right) {
+    if (str[left] === str[right]) {
+      left++
+      right--
+    } else if (!firstMismatch) {
+      firstMismatch = true
+
+      if (str[left+1] === str[right]) {
+        left++
+      } else if (str[right-1] === str[left]) {
+        right--
+      } else {
+        return false
+      }
+    } else {
+      return false
+    }
+  }
+
+  return true
+}
+
+console.log(removeOnePalindrome('aba'))
+console.log(removeOnePalindrome('abcba'))
+console.log(removeOnePalindrome('foobof'))
+console.log(removeOnePalindrome('abccab'))
