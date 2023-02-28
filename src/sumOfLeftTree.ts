06cd5a91608ea6a94386eaa80f68e19f45a6a0b1
Jonathan Fraimorice <jonathanfr@wix.com>
2023-03-01 00:46:30 +0200

    Sum of left tree

---
+++ src/sumOfLeftTree.ts
@@ -0,0 +1,43 @@
+// Given a binary tree, return the sum of all left leaves of the tree. Ex: Given the following tree…
+
+//     5
+//    / \
+//   2   12
+//      /  \
+//     3    8
+// return 5 (i.e. 2 + 3)
+// Ex: Given the following tree…
+
+//        2
+//       / \
+//     4    2
+//    / \
+//   3   9
+// return 3
+
+// IDEA: Traverse the tree in DFS and print only the leaves that are on the left
+
+interface TreeNode {
+    value: number,
+    right: TreeNode | null,
+    left: TreeNode | null
+}
+
+const sumOfLeftLeaves = (root: TreeNode | null | undefined): number => {
+    // Stopping conditions
+    if (!root) {
+        return 0
+    }
+    if (!root.left && !root.right) {
+        return root.value
+    }
+
+    return sumOfLeftLeaves(root.left) + sumOfLeftLeaves(root.right?.left)
+}
+
+const v: TreeNode = {
+    value: 5,
+    right: { value: 12, right: { value: 8, right: null, left: null }, left: { value: 3, right: null, left: null } },
+    left: { value: 2, right: null, left: null }
+}
+console.log(sumOfLeftLeaves(v))
