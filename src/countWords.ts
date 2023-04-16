6640eb172d2dd22198a7326c5602686f6ff115b9
Jonathan Fraimorice <jonathanfr@wix.com>
2023-04-16 23:01:31 +0300

    MaxWords

---
+++ src/countWords.ts
@@ -0,0 +1,39 @@
+console.log(countWords('this is the this marvelous'))
+console.log(maxWord('this is the this marvelous'))
+
+function countWords(str: string) {
+    const map = new Map<string, number>()
+
+    str.split(' ').forEach(word => {
+        if (map.has(word)) {
+            map.set(word, map.get(word) + 1)
+        } else {
+            map.set(word, 1)
+        }
+    })
+
+    return map
+}
+
+function maxWord(str: string) {
+    let maxCount = 0
+    let maxWord = ''
+    const map = new Map<string, number>()
+
+    str.split(' ').forEach(word => {
+        if (map.has(word)) {
+
+            map.set(word, map.get(word) + 1)
+        } else {
+            map.set(word, 1)
+        }
+
+        const count = map.get(word)
+        if (count > maxCount) {
+            maxWord = word
+            maxCount = count
+        }
+    })
+
+    return {maxWord, maxCount}
+}
