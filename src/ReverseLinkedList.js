fe2e6f6d5f5ad917a890daca7e7893eba241dbef
Jonathan Fraimorice <jonathanfr@wix.com>
2024-05-07 13:55:44 +0300

    Reverse Linked List

---
+++ src/ReverseLinkedList.js
@@ -0,0 +1,32 @@
+// Reverse Linked List
+// https://leetcode.com/problems/reverse-linked-list/description/
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+    if (head === null || !head.next) {
+        return head
+    }
+
+    let prev = null
+    let current = head
+    let next = current.next
+
+    while(!!current) {
+        current.next = prev
+        prev = current
+        current = next
+        next = current?.next
+    }
+
+    return prev
+};
