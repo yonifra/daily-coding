b5aec22f36dbf5d76b36f1a5f7006a0b7ba012e9
yonifra <yonifra@gmail.com>
2024-06-01 15:31:36 +0300

    House Robber typescript

---
+++ src/houseRobber.ts
@@ -0,0 +1,26 @@
+// House Robber - Medium
+// https://leetcode.com/problems/house-robber/
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var rob = function(nums: number[]): number {
+    if (!nums) return 0
+    if (nums.length === 1) {
+        return nums[0]
+    }
+    if (nums.length === 2) {
+        return Math.max(nums[0], nums[1])
+    }
+
+    const values = [nums[0], Math.max(nums[0], nums[1])]
+
+    for (let i = 2; i < nums.length; i++) {
+        values.push(Math.max(values[i-2] + nums[i], values[i-1]))
+    }
+
+    return values[values.length - 1]
+};
+
+console.log(rob([1,2,3,1])) // 4
