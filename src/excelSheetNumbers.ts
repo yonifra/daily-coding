19b88b304c5a97c36bb427a16faf663fd5c6e57e
yonifra <yonifra@gmail.com>
2024-06-12 23:19:33 +0300

    chore(question): :sparkles: Solved excel sheet convert to title

---
+++ src/excelSheetNumbers.ts
@@ -0,0 +1,24 @@
+// Source: https://leetcode.com/problems/excel-sheet-column-title/
+// Given a positive integer, return its corresponding column title as appear in an Excel sheet.
+// For example:
+// 1 -> A
+// 2 -> B
+// 3 -> C
+// ...
+// 26 -> Z
+// 27 -> AA
+// 28 -> AB
+
+function convertToTitle(columnNumber: number): string {
+    let result = '';
+
+    while (columnNumber > 0) {
+        columnNumber--;
+        result = String.fromCharCode(columnNumber % 26 + 65) + result;
+        columnNumber = Math.floor(columnNumber / 26);
+    }
+
+    return result;
+};
+
+console.log(convertToTitle(701)) // "ZY"
