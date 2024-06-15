1727f76009a5249a0fd9b9cea198bf8e6d52b9c7
yonifra <yonifra@gmail.com>
2024-06-15 17:04:02 +0300

    chore(question): :white_check_mark: Solved Contiguous Array

---
+++ src/contigiousArray.ts
@@ -0,0 +1,23 @@
+// Contiguous Array [Medium]
+// Source: https://leetcode.com/problems/contiguous-array/
+// Given a binary array nums, return the maximum length of a contiguous subarray with an equal number of 0 and 1.
+
+function findMaxLength(nums: number[]): number {
+    let count = 0;
+    let max = 0;
+    let map = new Map();
+    map.set(0, -1);
+    for (let i = 0; i < nums.length; i++) {
+        count += nums[i] === 0 ? -1 : 1;
+        if (map.has(count)) {
+            max = Math.max(max, i - map.get(count));
+        } else {
+            map.set(count, i);
+        }
+    }
+    return max;
+}
+
+console.log(findMaxLength([0,1]))
+console.log(findMaxLength([0,1,0]))
+console.log(findMaxLength([0,0,0,1,1,1,0]))
