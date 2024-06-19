d270f4105adadc6d8a6213812ae869bce5d0a2a6
Jonathan Fraimorice <jonathanfr@wix.com>
2024-06-20 00:07:10 +0300

    Group anagrams

---
+++ src/groupAnagrams.ts
@@ -0,0 +1,21 @@
+// Group Anagrams - [MEDIUM]
+// https://leetcode.com/problems/group-anagrams/
+// Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+// An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+function groupAnagrams(strs: string[]): string[][] {
+    const map = new Map<string, string[]>();
+
+    for (let str of strs) {
+        const sortedStr = str.split("").sort().join("");
+        if (!map.has(sortedStr)) {
+            map.set(sortedStr, [str]);
+        } else {
+            map.get(sortedStr).push(str);
+        }
+    }
+
+    return Array.from(map.values());
+}
+
+console.log(groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]));
