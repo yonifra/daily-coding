800911979ff0311f9b53281717b3006d88637abf
Jonathan Fraimorice <jonathanfr@wix.com>
2024-06-28 16:59:48 +0300

    more goodies

---
+++ src/houseRobberII.ts
@@ -0,0 +1,43 @@
+// House Robber II - [MEDIUM]
+// https://leetcode.com/problems/house-robber-ii/
+
+function rob(nums: number[]): number {
+    if (!nums) {
+        return 0;
+    }
+
+    if (nums.length === 1) {
+        return nums[0]
+    }
+
+    let rob1 = 0
+    let rob2 = 0
+    let max1 = 0
+    let max2 = 0
+
+    for (let i = 0; i < nums.length - 1; i++) {
+        let temp = Math.max(nums[i] + rob1, rob2)
+        rob1 = rob2;
+        rob2 = temp;
+    }
+
+    max1 = rob2;
+    rob1 = 0;
+    rob2 = 0;
+
+    for (let i = 1; i < nums.length; i++) {
+        let temp = Math.max(nums[i] + rob1, rob2)
+        rob1 = rob2;
+        rob2 = temp;
+    }
+
+    max2 = rob2;
+
+    return Math.max(max1, max2)
+};
+
+// Time complexity: O(n)
+// Space complexity: O(1)
+console.log(rob([2,3,2])) // 3
+console.log(rob([1,2,3,1])) // 4
+console.log(rob([1,2,3])) // 3
---
+++ src/longestSubstringNoDuplicates.ts
@@ -0,0 +1,40 @@
+// Longest Substring Without Repeating Characters - [MEDIUM]
+// Source: https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
+function lengthOfLongestSubstring(s: string): number {
+    if (!s || s.length === 0) {
+        return 0;
+    }
+
+    if (s.length === 1) {
+        return 1;
+    }
+
+    let left = 0;
+    let right = 1;
+    let maxLength = 1;
+
+    while (left < s.length) {
+        const substring = s.slice(left, right - left)
+        const set = new Set(substring.split(''))
+        console.log({size: set.size, substring});
+        if (set.size === substring.length) {
+            console.log('no duplicates');
+            // console.log({maxLength, right, left});
+            maxLength = Math.max(maxLength, right-left)
+            right++;
+        } else {
+            console.log('duplicate');
+            left++
+            // if (left === right) {
+            //     right++;
+            // }
+        }
+    }
+
+
+    // maxLength = Math.max(maxLength, right-left)
+    return maxLength;
+};
+
+console.log(lengthOfLongestSubstring('abcabcbb')) // 3
