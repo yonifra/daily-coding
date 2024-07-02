42c22144a40abba5aff448e2bcea412df762329c
Jonathan Fraimorice <yonifra@gmail.com>
2024-07-03 00:38:30 +0300

    feat: Add invertBinaryTree function

---
+++ src/invertBinaryTree.ts
@@ -0,0 +1,20 @@
+// Invert Binary Tree - EASY
+// Given the root of a binary tree, invert the tree, and return its root.
+// https://leetcode.com/problems/invert-binary-tree/
+
+var invertTree = function(root: TreeNode): TreeNode {
+    if (!root) {
+        return root
+    }
+
+    // invert
+    const left = root.left
+    const right = root.right
+    root.right = left
+    root.left = right
+
+    invertTree(root.right)
+    invertTree(root.left)
+
+    return root
+};
