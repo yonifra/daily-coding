51c0d7a156501ae8b00d6d7b4372a0351c90f7b0
Jonathan Fraimorice <yonifra@gmail.com>
2024-07-04 01:20:31 +0300

    feat: Add Fibonacci function with cache mechanism for better performance

---
+++ src/fibonacci.ts
@@ -0,0 +1,18 @@
+// Fibonacci with a cache mechanism for better performance and memory efficiency
+
+const cache = [0,1,1]
+
+function fibonacciWithCache(n: number): number {
+
+    if (cache[n] !== undefined) {
+        // Return cached value if it exists
+        return cache[n];
+    } else {
+        // Compute the Fibonacci number and store it in the cache
+        cache[n] = fibonacciWithCache(n - 1) + fibonacciWithCache(n - 2);
+        return cache[n];
+    }
+}
+
+console.log(fibonacciWithCache(140))
+console.log(fibonacciWithCache(30))
