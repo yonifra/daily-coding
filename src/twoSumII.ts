f2d66f65f8aec7d90d93652dd6dccaa5a24b83d1
Jonathan Fraimorice <jonathanfr@wix.com>
2024-07-04 14:48:51 +0300

    feat: Remove commented out code in twoSumII.ts

--- src/twoSumII.ts
+++ src/twoSumII.ts
@@ -27,16 +27,6 @@ function binarySearch(numbers: number[], start: number, end: number, target: num
     }
 }
 
-// function findMaxIndex(numbers: number[], target: number): number {
-//     for (let i = numbers.length - 1; i >= 1; i--) {
-//         if (numbers[i-1] < target) {
-//             return i
-//         }
-//     }
-
-//     return numbers.length - 1
-// }
-
 console.log(twoSum([2,7,11,15], 9)) // [0,1]
 console.log(twoSum([2,3,4], 6)) // [0,2]
 console.log(twoSum([-1,0], -1)) // [0,1]
