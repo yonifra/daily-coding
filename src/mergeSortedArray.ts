2ea01ff5cfdbf8f357523eb1b51146e76ae84736
Jonathan Fraimorice <yonifra@gmail.com>
2024-07-07 11:21:35 +0300

    feat: Implement mergeSortedArray function for merging two sorted arrays into one sorted array

---
+++ src/mergeSortedArray.ts
@@ -0,0 +1,40 @@
+// Merge Sorted Array - EASY
+// Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
+// The number of elements initialized in nums1 and nums2 are m and n respectively.
+// You may assume that nums1 has a size equal to m + n such that it has enough space to hold additional elements from nums2.
+// https://leetcode.com/problems/merge-sorted-array/
+
+/**
+ Do not return anything, modify nums1 in-place instead.
+ */
+ function merge(nums1: number[], m: number, nums2: number[], n: number): void {
+    if (m === 0) {
+        nums1[0] = nums2[0]
+    }
+
+    if (n === 0){
+        return
+    }
+    let right1 = m - 1;
+    let right2 = n - 1;
+    let leftMost = nums1.length - 1;
+
+    while (right1 >= 0 && right2 >= 0) {
+        if (nums1[right1] > nums2[right2]) {
+            nums1[leftMost] = nums1[right1]
+            leftMost--
+            right1--
+        } else {
+            nums1[leftMost] = nums2[right2]
+            right2--
+            leftMost--
+        }
+    }
+
+    for (let i = 0; i <= right2; i++) {
+        nums1[i] = nums2[i]
+    }
+};
+
+console.log(merge([1,2,3,0,0,0], 3, [2,5,6], 3)) // [1,2,2,3,5,6]
+console.log(merge([1], 1, [], 0)) // [1]
